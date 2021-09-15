2e7f79e5864746d474d229d337c517bca2a64fee
happ-in <gpqls304@naver.com>
2021-09-15 14:43:54 +0900

    #S05P21A401-106 Feat: Init

---
+++ backend/ssafy/src/main/java/com/billige/ssafy/SsafyApplication.java
@@ -0,0 +1,13 @@
+package com.billige.ssafy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsafyApplication.class, args);
+	}
+
+}
---
+++ backend/ssafy/src/test/java/com/billige/ssafy/SsafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billige.ssafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
