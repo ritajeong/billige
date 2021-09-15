df104718a36ff97bae84bc6cb505416eb6aa8508
DESKTOP-KVCQHCD\multicampus <minzminzm@gmail.com>
2021-09-15 13:13:00 +0900

    [S05P21A401-110] Design: Header && NavBottom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@
 import Layout from "./components/Layout/Layout";
 import Main from "./components/Layout/Main";
 import Location from "./components/Location";
+import Lent from "./components/Lent";
+import Detail from "./components/Detail";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
@@ -11,8 +13,10 @@ function App() {
       <Router>
         <Switch>
           <Layout>
-            <Route path="/location" component={Location} exact />
             <Route path="/" component={Main} exact />
+            <Route path="/location" component={Location} exact />
+            <Route path="/lent" component={Lent} exact />
+            <Route path="/detail" component={Detail} exact />
           </Layout>
         </Switch>
       </Router>
---
+++ frontend/src/components/Lent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Lent = () => {
+  return <div>대여하기</div>;
+};
+
+export default Lent;
