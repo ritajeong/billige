00708b8dac434565d41b4487094302e9ccff437b
DESKTOP-KVCQHCD\multicampus <minzminzm@gmail.com>
2021-09-17 10:15:10 +0900

    Feat: Update Calendar 대여하기 페이지의 캘린더와 대여일 반납일을 연동하였습니다 Resolves: S05P21A401-124

--- frontend/src/pages/Rent/Rent.js
+++ frontend/src/pages/Rent/Rent.js
@@ -3,36 +3,104 @@ import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css'; // theme css file
 import { DateRange } from 'react-date-range';
 import "./styles.css"
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 
 const Rent = () => {
+
+const stringToNum = {
+	Jan: 1,
+	Feb: 2,
+	Mar: 3,
+	Apr: 4,
+	May: 5,
+	Jun: 6,
+	Jul: 7,
+	Aug: 8,
+	Sep: 9,
+	Oct: 10,
+	Nov: 11,
+	Dec: 12
+}
+
+const today = new Date().toString().split(' ');
 	
 const [state, setState] = useState([
 	{
 		startDate: new Date(),
-		endDate: null,
+		endDate: new Date(),
 		key: 'selection',
 		color: '#C5D3CC',
 		preview: {
-			color: '#E96F02',
+			color: '#C5D3CC',
 		}
 	}
 ]);
 
+const [rentalStartYear, setrentalStartYear] = useState(today[3]);
+const [rentalStartMonth, setrentalStartMonth] = useState(stringToNum[today[1]]);
+const [rentalStartDay, setrentalStartDay] = useState(today[2]);
+const [rentalEndYear, setrentalEndYear] = useState(today[3]);
+const [rentalEndMonth, setrentalEndMonth] = useState(stringToNum[today[1]]);
+const [rentalEndDay, setrentalEndDay] = useState(today[2]);
+
+const onChangeDate = (item) => {
+	setState([item.selection]);
+	const y1 = item.selection['startDate'].toString().split(' ')[3];
+	const m1 = stringToNum[item.selection['startDate'].toString().split(' ')[1]];
+	const d1 = item.selection['startDate'].toString().split(' ')[2];
+	const y2 = item.selection['endDate'].toString().split(' ')[3];
+	const m2 = stringToNum[item.selection['endDate'].toString().split(' ')[1]];
+	const d2 = item.selection['endDate'].toString().split(' ')[2];
+	setrentalStartYear(y1);
+	setrentalStartMonth(m1);
+	setrentalStartDay(d1);
+	setrentalEndYear(y2);
+	setrentalEndMonth(m2);
+	setrentalEndDay(d2);
+}
+
   return (
 		<div>
-		<div className="calendar">
-		<DateRange
-		editableDateInputs={true}
-		onChange={item => setState([item.selection])}
-		moveRangeOnFirstSelection={false}
-		ranges={state}
-/>
+			<div className="calendar">
+			<DateRange
+			editableDateInputs={true}
+			onChange={onChangeDate}
+			// onChange={item => setState([item.selection])}
+			moveRangeOnFirstSelection={false}
+			ranges={state}
+	/>
+			</div>
+			<br />
+			<div className="rent-date">
+				<div className="rent-raental-box">
+					<div>대여일</div>
+					<br />
+					<div className="rent-raental-date">{rentalStartYear}년 {rentalStartMonth}월 {rentalStartDay}일</div>
+				</div>
+				<div className="rent-return-box">
+					<div>반납일</div>
+					<br />
+					<div className="rent-return-date">{rentalEndYear}년 {rentalEndMonth}월 {rentalEndDay}일</div>
+				</div>
 		</div>
-		<div className="rent-date">
-
-		<div className="rent-raental-date"></div>
-		<div className="rent-raental-date"></div>
+		<div className="rent-estimated-price-box">
+				<div className="rent-estimated-comment">
+				예상주문금액
+				</div>
+				<div className="rent-nbutton-price">
+					<div className="rent-next-button">
+						<FontAwesomeIcon icon={faArrowCircleRight} size="2x"/>
+					</div>
+					<div className="rent-price-krw">
+						<div className="rent-estimated-price">
+							200,000&nbsp;
+						</div>
+						<div className="rent-estimated-krw">
+							원
+						</div>
+					</div>
+				</div>
 		</div>
 		</div>
 	)
