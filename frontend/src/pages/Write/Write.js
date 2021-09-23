bc1bec4dc90e8b16538db3d9a7e1593b452bd581
DESKTOP-KVCQHCD\multicampus <minzminzm@gmail.com>
2021-09-23 17:19:31 +0900

    Feat: Deploy Blockchain 롭슨 테스트넷 네트워크를 통해 erc20 토큰을 배포함 Resolves: S05P21A401-148

--- frontend/src/pages/Write/Write.js
+++ frontend/src/pages/Write/Write.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Input } from 'semantic-ui-react';
 
 const Write = () => {
-  return <div>글쓰기 페이지</div>;
-};
+
+  return (
+    <div>
+      <div>상품명</div>
+      <Input placeholder='Search...' />
+      <div>이미지 업로드</div>
+      <div>
+      </div>
+      <div>카테고리 설정</div>
+      <div>대여 가격(1일)</div>
+      <div>상품 설명</div>
+    </div>
+  )};
 
 export default Write;
