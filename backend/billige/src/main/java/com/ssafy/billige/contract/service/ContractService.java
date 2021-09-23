36c03104d82fc337c677d95b4b1940b90d8c57d0
happ-in <gpqls304@naver.com>
2021-09-23 11:18:51 +0900

    Feat: Feat contract item 제품 대여하기 Resolves: S05P21A401-116

--- backend/billige/src/main/java/com/ssafy/billige/contract/service/ContractService.java
+++ backend/billige/src/main/java/com/ssafy/billige/contract/service/ContractService.java
@@ -1,4 +1,7 @@
 package com.ssafy.billige.contract.service;
 
+import com.ssafy.billige.contract.dto.ContractRequest;
+
 public interface ContractService {
+	void contractSave(ContractRequest contractRequest);
 }
--- backend/billige/src/main/java/com/ssafy/billige/contract/service/impl/ContractServiceImpl.java
+++ backend/billige/src/main/java/com/ssafy/billige/contract/service/impl/ContractServiceImpl.java
@@ -1,8 +1,42 @@
 package com.ssafy.billige.contract.service.impl;
 
+import com.ssafy.billige.contract.domain.Contract;
+import com.ssafy.billige.contract.dto.ContractRequest;
+import com.ssafy.billige.contract.repository.ContractRepository;
 import com.ssafy.billige.contract.service.ContractService;
+import com.ssafy.billige.item.domain.ActiveStatus;
+import com.ssafy.billige.item.domain.Item;
+import com.ssafy.billige.item.repository.ItemRepository;
+import com.ssafy.billige.user.repository.UserRepository;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ContractServiceImpl implements ContractService {
+
+	private final ContractRepository contractRepository;
+	private final UserRepository userRepository;
+	private final ItemRepository itemRepository;
+	private final ModelMapper modelMapper;
+
+	@Override
+	@Transactional
+	public void contractSave(ContractRequest contractRequest) {
+		Contract contract = modelMapper.map(contractRequest, Contract.class);
+		contract.setUser(userRepository.findById(contractRequest.getUid())
+			.orElseThrow(() -> new IllegalArgumentException("대여하는 회원의 정보를 확인해주세요.")));
+		Item item = itemRepository.findById(contractRequest.getItemId())
+			.orElseThrow(() -> new IllegalArgumentException("대여하려는 제품을 확인해주세요."));
+		if (item.getIsActive().equals(ActiveStatus.Y)) {
+			throw new IllegalArgumentException("대여가 비활성화된 제품입니다.");
+		}
+		contract.setItem(item);
+		contractRepository.save(contract);
+	}
 }
