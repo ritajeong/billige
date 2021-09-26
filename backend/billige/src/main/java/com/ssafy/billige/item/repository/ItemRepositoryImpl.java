dea6733024d09de34c7dfa8a8c99959a24ffb1d8
happ-in <gpqls304@naver.com>
2021-09-26 19:52:40 +0900

    Refactor: Refactor search for activated items only 활성화된 대여품 조회 Resolves: S05P21A401-141

--- backend/billige/src/main/java/com/ssafy/billige/item/repository/ItemRepositoryImpl.java
+++ backend/billige/src/main/java/com/ssafy/billige/item/repository/ItemRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.ssafy.billige.item.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.billige.item.domain.ActiveStatus;
 import com.ssafy.billige.item.dto.response.ItemResponse;
+
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
@@ -12,26 +14,27 @@ import static com.ssafy.billige.bookmark.domain.QBookmark.*;
 import static com.ssafy.billige.utils.StringUtils.*;
 
 @RequiredArgsConstructor
-public class ItemRepositoryImpl implements ItemRepositoryCustom{
+public class ItemRepositoryImpl implements ItemRepositoryCustom {
 
-    private final JPAQueryFactory queryFactory;
+	private final JPAQueryFactory queryFactory;
 
-    @Override
-    public List<ItemResponse> findAllItemResponseList(int offset) {
-        return queryFactory
-                .select(Projections.constructor(ItemResponse.class,
-                        item.itemId,
-                        item.itemname,
-                        item.position,
-                        item.price,
-                        item.modifiedTime,
-                        bookmark.count().as("item_id")))
-                .from(item)
-                .leftJoin(bookmark).on(item.itemId.eq(bookmark.bookmarkId.itemId))
-                .groupBy(bookmark.bookmarkId.itemId)
-                .orderBy(item.modifiedTime.desc())
-            .limit(LIMIT)
-            .offset(offset)
-            .fetch();
-    }
+	@Override
+	public List<ItemResponse> findAllItemResponseList(int offset) {
+		return queryFactory
+			.select(Projections.constructor(ItemResponse.class,
+				item.itemId,
+				item.itemname,
+				item.position,
+				item.price,
+				item.modifiedTime,
+				bookmark.count().as("item_id")))
+			.from(item)
+			.where(item.isActive.eq(ActiveStatus.Y))
+			.leftJoin(bookmark).on(item.itemId.eq(bookmark.bookmarkId.itemId))
+			.groupBy(bookmark.bookmarkId.itemId)
+			.orderBy(item.modifiedTime.desc())
+			.limit(LIMIT)
+			.offset(offset)
+			.fetch();
+	}
 }
