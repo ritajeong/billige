5c1e94104e350d97267939134f15f2282eb1fe10
jinho_pca <jinho_pca@naver.com>
2021-09-27 10:47:31 +0900

    Fix: Update StringUtils 충돌해결

--- backend/billige/src/main/java/com/ssafy/billige/utils/StringUtils.java
+++ backend/billige/src/main/java/com/ssafy/billige/utils/StringUtils.java
@@ -7,12 +7,10 @@ public class StringUtils {
 	public static final int LIMIT = 20;
 
 	public static final String CONTRACT = "/api/contract";
-<<<<<<< HEAD
 	public static final String USER = "api/user";
+	public static final String ITEM = "/api/item";
 	// JWT String
 	public static final String AUTH_HEADER = "Authentication";
 	public static final String TOKEN_TYPE = "BEARER";
-=======
-	public static final String ITEM = "/api/item";
->>>>>>> develop
+
 }
