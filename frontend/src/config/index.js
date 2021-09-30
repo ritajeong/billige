4fae083f2a8ce354534db6f3174edcd67914e458
김민재 <minzminzm@gmail.com>
2021-09-30 09:49:06 +0900

    Feat: Add getWeb3.js infura와 통신하는 코드를 작성했습니다. Resolves: S05P21A401

---
+++ frontend/src/config/index.js
@@ -0,0 +1,7 @@
+// export const abi = require("../build/contracts/VoteManager.json").abi;
+
+// export const contractAddress = process.env.CONTRACT_ADDRESS;
+
+// export const gethHost = process.env.BLOCKCHAIN_PROVIDER;
+
+// export const coinbase = process.env.COINBASE;
