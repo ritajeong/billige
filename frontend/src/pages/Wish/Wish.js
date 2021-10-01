4b276c1f3230715c967d105c72d53dac4f0060fa
happ-in <gpqls304@naver.com>
2021-10-01 16:19:03 +0900

    Feat: Feat Wish page 찜 페이지 제작 Resolves: S05P21A401-187

--- frontend/src/pages/Wish/Wish.js
+++ frontend/src/pages/Wish/Wish.js
@@ -1,7 +1,42 @@
 import React from "react";
-
+import profile from "../../assets/image/user.png";
+import wishOn from "../../assets/icons/wish-on.png";
+import "./Wish.css";
 const Wish = () => {
-  return <div>찜 페이지</div>;
+  const wishProduct = [
+    {
+      itemname: "비모 피규어",
+      price: "4000",
+      address: "서울특별시 강남구",
+      image: "",
+      book: true,
+    },
+    {
+      itemname: "버즈 플러스",
+      price: "4000",
+      address: "서울특별시 강남구",
+      image: "",
+      book: true,
+    },
+  ];
+  return wishProduct.map((product, idx) => {
+    return (
+      <div key={idx}>
+        <div className="wish-item-list">
+          <img src={profile} className="wish-item-icon"></img>
+          <div className="wish-item-vertical">
+            <div className="wish-item-title">{product.itemname}</div>
+            <span>{product.address}</span>
+            <img src={wishOn} className="wish-item-bookmark" />
+            <div className="wish-item-price">{product.price} 원</div>
+          </div>
+          <div>
+            <button className="wish-item-borrow">대여하기</button>
+          </div>
+        </div>
+      </div>
+    );
+  });
 };
 
 export default Wish;
