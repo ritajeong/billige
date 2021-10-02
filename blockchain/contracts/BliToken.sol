f922adff7b10cf7be3ec2f5c519661144b806a0c
김민재 <minzminzm@gmail.com>
2021-10-02 17:01:42 +0900

    Feat: Update Bli Coin 빌리코인 재배포 완료. 이제 이 컨트랙트 주소로 물물 교환이 이뤄질 것이다. Resolves: S05P21A401-188

--- blockchain/contracts/BliToken.sol
+++ blockchain/contracts/BliToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract BliToken is ERC20{
-  uint public INITIAL_SUPPLY = 120000*10**18;
+  uint public INITIAL_SUPPLY = 120000*100**18;
 
   constructor() public ERC20("Bli Token","BLI"){
     _mint(msg.sender, INITIAL_SUPPLY);
