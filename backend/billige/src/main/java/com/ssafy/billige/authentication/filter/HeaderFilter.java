4161b04e7ae5c1ec30e1f550d26b01fcc91e963f
happ-in <gpqls304@naver.com>
2021-10-03 03:31:43 +0900

    Fix: Fix CORS CORS 수정

--- backend/billige/src/main/java/com/ssafy/billige/authentication/filter/HeaderFilter.java
+++ backend/billige/src/main/java/com/ssafy/billige/authentication/filter/HeaderFilter.java
@@ -9,14 +9,14 @@ public class HeaderFilter implements Filter {
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         final HttpServletResponse res = (HttpServletResponse) response;
         res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+        res.setHeader("Access-Control-Allow-Methods", "*");
         res.setHeader("Access-Control-Max-Age", "3601");
         res.setHeader(
                 "Access-Control-Allow-Headers",
-                "X-Requested-With, Content-Type, Authorization, X-XSRF-token"
+                "X-Requested-With, Content-Type, Authentication, X-XSRF-token"
         );
         res.setHeader("Access-Control-Allow-Credentials", "false");
-        res.setHeader("Access-Control-Expose-Headers", "Authorization");
+        res.setHeader("Access-Control-Expose-Headers", "Authentication");
         chain.doFilter(request, response);
     }
 }
