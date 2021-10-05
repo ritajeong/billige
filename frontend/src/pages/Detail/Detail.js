402628f08cbd1b43109cc09f416132100be17d0f
김민재 <minzminzm@gmail.com>
2021-10-06 01:58:23 +0900

    Feat: Upload Image 게시글 작성할 때 이미지 등록함 Resolves: S05P21A401-202

--- frontend/src/pages/Detail/Detail.js
+++ frontend/src/pages/Detail/Detail.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
-import Product from '../../assets/image/product.png'
 import User from '../../assets/image/user.png'
 import './Detail.css'
 import unlikeIcon from '../../assets/icons/wish.png'
