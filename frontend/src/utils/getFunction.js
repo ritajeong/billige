6f3505167c16fa3c13f3d56c50ee743108ab0e06
김민재 <minzminzm@gmail.com>
2021-10-07 00:09:55 +0900

    Feat: Add User Image 사용자가 사진을 변경할 수 있도록 해주었어요

--- frontend/src/utils/getFunction.js
+++ frontend/src/utils/getFunction.js
@@ -16,13 +16,6 @@ export class getFunction {
   static myApples;
 
   constructor() {
-
-    // this.state = {
-    //   shopInstance: null, // shopInstance 추가
-    //   myAccount: null,    // myAccount 추가
-    //   myApples: 0,  // myApples 추가
-    //   web3: null
-    // };
   }
 	
   // static web3 = new Web3(gethHost);
@@ -74,36 +67,9 @@ export class getFunction {
     try{
       this.myAccount = await this.web3.eth.getAccounts();
       this.shopInstance = await shop.deployed();
-      // await this.web3.eth.getAccounts((error, accounts) => {
-      //   console.log(7);
-        // shop.deployed().then(instance => {
-        //   console.log(77);
-        //   this.shopInstance = instance;
-        //   this.myAccount = accounts[0];
-        // })
-      // })
     } catch {
       console.log("err")
     }
-
-    // await this.web3.eth.getAccounts((error, accounts) => {
-    //   console.log(7)
-    //   console.log(accounts)
-    //   if (!error) {
-    //     shop.deployed().then(instance => {
-    //       // console.log(instance);
-    //       console.log(77);
-    //       // console.log(instance.buyApple);
-    //       this.shopInstance = instance;
-    //       this.myAccount = accounts[0];
-
-    //       // const contract = new Web3Client.eth.Contract(minABI, tokenAddress);
-    //       // this.setState({ shopInstance: instance, myAccount: accounts[0] });
-    // //       console.log(this.state.myAccount);
-    //       // this.updateMyApples();
-    //     });
-    //   }
-    // });
   }
 
   static async getBliCoin(){
@@ -131,112 +97,4 @@ export class getFunction {
     }
     return getBalance();
   }
-
-  // 회원가입 시 실행
-  // static async createAccount() {
-  //   const web3 = this.web3;
-
-  //   console.log("account creating start");
-  //   const account = await web3.eth.personal.newAccount("ethereum"); // temp password: ethereum
-  //   console.log("account: ", account);
-  //   console.log("created!");
-
-  //   localStorage.myData = JSON.stringify({
-  //     address: account,
-  //   });
-  //   await this.unlockAccount();
-  //   return account;
-  // }
-
-  // static async unlockAccount() {
-  //   const web3 = this.web3;
-  //   await web3.eth.personal.unlockAccount(JSON.parse(localStorage.myData).address, "ethereum", 0); // unlock inf time
-  // }
-
-  // static async getBalance() {
-  //   const web3 = this.web3;
-  //   return await web3.eth.getBalance(JSON.parse(localStorage.myData).address);
-  // }
-
-  // static async receiveBalance() {
-  //   const web3 = this.web3;
-  //   const ether = web3.utils.toWei("1");
-  //   const currentBalance = await this.web3.eth.getBalance(JSON.parse(localStorage.myData).address);
-
-  //   if (parseInt(currentBalance) < parseInt(ether)) {
-  //     const rs = await web3.eth.sendTransaction({
-  //       from: coinbase, // main miner. unlocked.
-  //       to: JSON.parse(localStorage.myData).address,
-  //       value: ether,
-  //     });
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
-  // static async call(method, args) {
-  //   const rs = await method(...args).call({
-  //     from: JSON.parse(localStorage.myData).address,
-  //   });
-  //   return rs;
-  // }
-
-  // // 노드에서 계정을 관리할 경우
-  // static async send(method, args) {
-  //   // ether가 모자랄 경우 수급
-  //   console.log("get balance: ", await this.receiveBalance());
-
-  //   const account = JSON.parse(localStorage.myData).address;
-
-  //   try {
-  //     const rs = await method(...args).send({
-  //       from: account,
-  //     });
-  //     return rs;
-  //   } catch (err) {
-  //     console.log("send error: ", err);
-  //   }
-  // }
-
-  // // 로컬에서 계정을 관리할 경우 서명 후 전송
-  // static async signAndSend(method, args, callback) {
-  //   const web3 = this.web3;
-  //   const privateKey = JSON.parse(localStorage.myData).privateKey;
-  //   const account = JSON.parse(localStorage.myData).address;
-
-  //   // 트랜잭션 데이터
-  //   const txBuilder = method(...args);
-  //   const encodedTx = txBuilder.encodeABI();
-
-  //   // 기타 데이터
-  //   const nonce = await web3.eth.getTransactionCount(account);
-  //   const block = await web3.eth.getBlock("latest");
-  //   const gasLimit = block.gasLimit;
-  //   const gasPrice = (await web3.eth.getGasPrice()) * 1.5;
-
-  //   const txObject = {
-  //     nonce: web3.utils.toHex(nonce),
-  //     gasLimit,
-  //     gasPrice,
-  //     data: encodedTx,
-  //     from: JSON.parse(localStorage.myData).address,
-  //     to: contractAddress,
-  //   };
-
-  //   // sign
-  //   const rs = await web3.eth.accounts.signTransaction(txObject, privateKey, async (error, signedTx) => {
-  //     if (error) {
-  //       console.log("error!", error);
-  //     } else {
-  //       // send
-  //       try {
-  //         const _rs = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  //         callback(_rs);
-  //       } catch (err) {
-  //         console.log("run error!", err);
-  //       }
-  //     }
-  //   });
-  //   console.log("rs!!", rs);
-  // }
 }
