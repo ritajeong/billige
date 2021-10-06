644bd3c5b34332eb580c19e8dc45405d751d9821
happ-in <gpqls304@naver.com>
2021-10-06 11:29:33 +0900

    Fix: Fix bug

---
+++ backend/billige/src/test/java/com/ssafy/billige/BilligeApplicationTest.java
@@ -0,0 +1,15 @@
+package com.ssafy.billige;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilligeApplicationTest {
+
+	@Test
+	public void contextloads() {
+
+	}
+}
