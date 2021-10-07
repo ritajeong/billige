96c50f1a37099fccb48cefec5c22cc7040a88b4c
happ-in <gpqls304@naver.com>
2021-10-07 15:44:20 +0900

    Fix: Edit Main page

--- frontend/src/pages/Main/Main.js
+++ frontend/src/pages/Main/Main.js
@@ -7,36 +7,43 @@ import "./Main.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import ThumbNail from "../../components/ThumbNail/ThumbNail";
-import axios from 'axios';
-import SearchInput from "../../components/SearchInput/SearchInput"
+import axios from "axios";
+import SearchInput from "../../components/SearchInput/SearchInput";
 
 const Main = () => {
-  const [nearProduct, setNearProduct] = useState([])
+  const [nearProduct, setNearProduct] = useState([]);
+  const [rentProduct, setRentProduct] = useState([]);
+  const token = JSON.parse(window.localStorage.getItem("token"));
 
   useEffect(() => {
-
-    const token = JSON.parse(window.localStorage.getItem('token'))
     axios
       .get(`${process.env.REACT_APP_SERVER_BASE_URL}/api/item/list`)
       .then((response) => {
-        setNearProduct(response.data)
+        setNearProduct(response.data);
+        console.log(response.data);
       })
       .catch((error) => {
         console.log(error);
-      })
+      });
 
-  }, [])
+    axios
+      .get(`${process.env.REACT_APP_SERVER_BASE_URL}/api/contract/rent`, {
+        headers: {
+          Authentication: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        setRentProduct(response.data);
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   const NextArrow = (props) => {
     const { className, style, onClick } = props;
-    return (
-      <img
-        src={arrow}
-        alt="arrow"
-        className={className}
-        style={{ ...style, display: "block" }}
-        onClick={onClick}
-      />
-    );
+    return <img src={arrow} alt="arrow" className={className} style={{ ...style, display: "block" }} onClick={onClick} />;
   };
   const settings = {
     dots: true,
@@ -66,8 +73,8 @@ const Main = () => {
     ],
   };
 
-  const productCarousel = () => {
-    return nearProduct.map((product, idx) => {
+  const productCarousel = (productItem) => {
+    return productItem.map((product, idx) => {
       return (
         <div key={idx}>
           <ThumbNail product={product} />
@@ -78,29 +85,29 @@ const Main = () => {
   return (
     <div className="main">
       {/* <Input className="main-search" icon="search" iconPosition="left" /> */}
-      <SearchInput/>
+      <SearchInput />
       <Slider {...settings}>
         <div className="carousel-page">
-          <h3>한 번 쓰고 말건데</h3>
+          <h3>여기에</h3>
         </div>
         <div className="carousel-page">
-          <h3>사기는 아깝고</h3>
+          <h3>무엇을</h3>
         </div>
         <div className="carousel-page">
-          <h3>안쓰는 물건인데</h3>
+          <h3>쓰면</h3>
         </div>
         <div className="carousel-page">
-          <h3>버리기는 아까울 때</h3>
+          <h3>좋을까요</h3>
         </div>
         <div className="carousel-page">
-          <h3>빌리지하세요</h3>
+          <h3>?</h3>
         </div>
       </Slider>
 
       <div className="main-near-product">
         <h4>가까운 위치에 있는 물건 소개</h4>
 
-        <Slider {...responsiveSettings}>{productCarousel()}</Slider>
+        <Slider {...responsiveSettings}>{productCarousel(nearProduct)}</Slider>
       </div>
 
       <div className="main-current-rent">
@@ -111,9 +118,8 @@ const Main = () => {
           </Link>
         </div>
 
-        <Slider {...responsiveSettings}>{productCarousel()}</Slider>
+        <Slider {...responsiveSettings}>{productCarousel(rentProduct)}</Slider>
       </div>
-
     </div>
   );
 };
